53801853689460f62ce6ec6207465acd33b7c5f0
swlee <windnc@gmail.com>
2013-07-14 21:41:02 +0900

    dev

--- example/ustringdic_test.cpp
+++ example/ustringdic_test.cpp
@@ -6,8 +6,30 @@ void Test1()
 	CUStringDic dic;
 	dic.Clear();
 
-	if( dic.Binarize( "dic.sort", "dic" ) == false ) {
+	if( dic.Binarize( "tiny.txt", "dic" ) == false ) {
 		fprintf( stderr, "fail\n" );
+	}
+
+}
+
+void Test2()
+{
+	CUStringDic dic;
+	char *prefix = "dic";
+	if( dic.LoadBinary( prefix ) == false ) {
+		fprintf( stderr, "fail to load\n" );
+	}
+
+	//char *str = "whale/NN";
+	char *str = "6.25744/CD";
+	//char *str = "흙/NNG";
+	//char *str = "달러/NNB";
+	CUString result;
+	if( dic.GetValue( str, result ) == true ) {
+		fprintf( stderr, "%s\n%s\n", str, result.GetStr() );
+	}
+	else {
+		fprintf( stderr, "fail to get value: %s\n", str );
 	}
 
 }
@@ -15,6 +37,7 @@ void Test1()
 int main()
 {
 	Test1();
+	Test2();
 }
 
 // EOF ㅁ
